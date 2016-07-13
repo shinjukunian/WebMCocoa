c537b557e2bf07b4386442af9659c705799464ba
Morten Bertz <morten@telethon.jp>
2016-07-13 19:36:56 +0900

    Initial Commit

---
+++ VPX/VPX.h
@@ -0,0 +1,19 @@
+//
+//  VPX.h
+//  VPX
+//
+//  Created by Morten Bertz on 7/13/16.
+//  Copyright © 2016 telethon k.k. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for VPX.
+FOUNDATION_EXPORT double VPXVersionNumber;
+
+//! Project version string for VPX.
+FOUNDATION_EXPORT const unsigned char VPXVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VPX/PublicHeader.h>
+
+
